f6f2fdba9dbdd8bc5c6fd3e5e8f3d28a11cbae5a
Patturisrinivas <srinivaspatturi210.com>
2025-01-04 08:05:46 +0530

    SSH-Learn

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=RAM
+PERSON2=Laxman
+
+echo "$PERSON1:: Hir $PERSON2"
+echo "$PERSON2:: hollow Ram"
+echo "$PERSON1:: I am doing fine"
+echo "$PERSON2:: Happy New Year"
+echo "$PERSON1:: wish you the same"
