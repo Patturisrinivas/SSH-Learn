8b784d4ff426e16446390ff0186f1683cb5b5c98
Patturisrinivas <srinivaspatturi210.com>
2025-01-04 09:46:22 +0530

    SSH-Lear

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -3,9 +3,10 @@
 NUMBER=$1
 
 # -gt, -lt, -eq, -ge, le
+
 if [ $NUMBER -gt 100 ] 
 than 
     echo "Given number is greater than 100"
 else
-    echo "Given number is less than or 100"
+    echo "Given number is less than or equal to 100"
 fi
